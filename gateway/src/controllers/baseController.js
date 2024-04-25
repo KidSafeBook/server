883dab7dc2c7cfbe5ad09d16d3d9a50f7ab24b51
ChrisJohDev <chris@chrisjohannesson.com>
2024-04-25 16:40:35 +0200

    Major structural refactoring accomodating GraphQL for more efficient control and service

--- gateway/src/controllers/baseController.js
+++ gateway/src/controllers/baseController.js
@@ -5,22 +5,28 @@ const baseControllerGet = (req, res, next) => {
   // console.log("baseControllerGet called");
   const post = {
     postId: "wqeqw123",
+    username: "User 1",
     authorId: "1234",
     title: "Post 1 title here ...",
     content: "Post content here ...",
     comments: [],
     likes: 2,
-    dislikes: 1
+    dislikes: 1,
+    creationDate: new Date(Date.now() - 1000 * 60 * 60 * 24 * 2),
+    updateDate: new Date(Date.now() - 1000 * 60 * 60 * 24 * 1)
   };
 
   const post1 = {
     postId: "wqeqw456",
+    username: "User 2",
     authorId: "6543",
     title: "Post 2 title here ...",
     content: "Post new post content here ...",
     comments: [],
     likes: 3,
-    dislikes: 5
+    dislikes: 5,
+    creationDate: new Date(Date.now() - 1000 * 60 * 60 * 24 * 2),
+    updateDate: null
   };
 
   const response = [post, post1]
